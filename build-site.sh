81c5c22aa0c91bd2bb493d9e2d73102880b8d8af
Wei Ouyang <oeway007@gmail.com>
2020-06-24 13:10:58 +0200

    add build-site script

---
+++ build-site.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/bioimage-io/bioimage.io.git bioimage.io
+cp site.config.json ./bioimage.io
+cp -R public ./bioimage.io
+cd ./bioimage.io
+npm ci
+npm run build
+cp -R ./dist ../
