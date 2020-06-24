945b9195ed7db5aa4d7ba6e78e347a49f6711229
Wei Ouyang <oeway007@gmail.com>
2020-06-24 13:15:11 +0200

    Add comment

--- build-site.sh
+++ build-site.sh
@@ -1,7 +1,8 @@
+# This file is used for building PR preview with Netlify
 git clone https://github.com/bioimage-io/bioimage.io.git bioimage.io
 cp site.config.json ./bioimage.io
 cp -R public ./bioimage.io
 cd ./bioimage.io
 npm ci
 npm run build
-cp -R ./dist ../
+mv ./dist ../
