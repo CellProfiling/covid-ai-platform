5a1917933e03e7b75d7666f14078e05e25a9a11b
Wei Ouyang <oeway007@gmail.com>
2020-06-25 08:46:24 +0200

    Fix build script for netlify preview (#14)
    * Fix build script for netlify preview
    
    * fix build script

--- build-site.sh
+++ build-site.sh
@@ -1,5 +1,14 @@
 # This file is used for building PR preview with Netlify
+if [ -d "bioimage.io" ]; then
+cd bioimage.io
+git stash
+git fetch
+git checkout master
+git reset --hard origin/master
+cd ../
+else
 git clone https://github.com/bioimage-io/bioimage.io.git bioimage.io
+fi
 cp site.config.json ./bioimage.io
 cp -R public ./bioimage.io
 cd ./bioimage.io
