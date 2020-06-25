c09b15fc2333a47745d05228244a591d84424555
Wei Ouyang <oeway007@gmail.com>
2020-06-25 08:43:15 +0200

    fix build script

--- build-site.sh
+++ build-site.sh
@@ -1,7 +1,10 @@
 # This file is used for building PR preview with Netlify
 if [ -d "bioimage.io" ]; then
 cd bioimage.io
-git pull
+git stash
+git fetch
+git checkout master
+git reset --hard origin/master
 cd ../
 else
 git clone https://github.com/bioimage-io/bioimage.io.git bioimage.io
